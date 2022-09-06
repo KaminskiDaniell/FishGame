eb0c4f4e55b61fea0690924ef818bdde1e853627
TheGoldyOne <kubazlotek@gmail.com>
2022-09-06 19:11:21 +0200

    a

--- main.js
+++ main.js
@@ -1,84 +1,112 @@
-let array = []
-
 document.addEventListener('mousemove', function (e) {
-    let body = document.querySelector('body')
-    let circle = document.getElementById('ryba')
 
+    const ImageOffsetX = - 60;
+    const ImageOffsetY = - 50;
 
-    let left = e.clientX;
-    let top = e.clientY;
+    const ryba = document.getElementById('ryba');
 
-    if (e.clientX - 60 + "px" < circle.style.left) {
-        circle.classList.remove("rotate");
+    if (`${e.clientX + ImageOffsetX}px` < ryba.style.left) {
+        ryba.classList.remove('rotate');
     } else {
-        circle.classList.add("rotate");
+        ryba.classList.add('rotate');
     }
 
-    circle.style.left = left - 60 + 'px';
-    circle.style.top = top - 50 + 'px';
-
-    Ryba.checkForJedzenie(array);
+    ryba.style.left = `${e.clientX + ImageOffsetX}px`;
+    ryba.style.top = `${e.clientY + ImageOffsetY}px`;
 });
 
-class Ryba {
-    ryba
-
-    constructor(element, imageSrc) {
-        this.ryba = document.getElementById(element)
-        this.image = imageSrc
-        let image = new Image()
-        image.src = imageSrc;
-        image.classList.add('ryba');
-        image.id = 'ryba'
-
-        document.querySelector('body').appendChild(image)
-        // document.getElementById(element).outerHTML = `<img src="${imageSrc}" alt="ryba" class="ryba" id="ryba">`
-    }
-
-    static checkForJedzenie(array) {
-        const ryba = document.getElementById('ryba')
-        console.log(ryba)
-        array.forEach(e => {
-            console.log([e.offsetX, ryba.offsetX])
-            if (e.offsetX - 20 < ryba.offsetX && e.offsetX + 20 > ryba.offsetX) {
-                console.log("jestes blisko zarcia")
-            }
-        })
-    }
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
-
-new Ryba('ryba', 'ryba.png')
-
-// new Ryba('ryba', 'https://png.clipart.me/previews/4fe/fish-outline-clip-art-43153.jpg')
 
 
 class Snack {
     constructor(size) {
-        let x = randomBetween(1, 100)
-        let y = randomBetween(1, 100)
-        // console.log(`X: ${x}, Y: ${y}, SIZE: ${size}`)
-        this.spawnSnack(x, y, size)
+        let x = randomBetween(1, 100);
+        let y = randomBetween(1, 100);
+        return this.spawnSnack(x, y, size);
     }
 
     spawnSnack(X, Y, size) {
-        let image = new Image()
-        image.src = 'https://cdn.shopify.com/s/files/1/2555/1168/files/information-worm_2048x.png?v=1632120158'
+        let image = new Image();
+        image.src =
+            'https://cdn.shopify.com/s/files/1/2555/1168/files/information-worm_2048x.png?v=1632120158';
         image.style.top = `${X}vh`;
         image.style.position = 'absolute';
         image.style.left = `${Y}vw`;
-        image.style.width = "auto";
+        image.style.width = 'auto';
         image.style.height = `${size}vh`;
         image.id = `${Date.now()}`;
-        document.querySelector('body').appendChild(image)
-        array.push(image);
+        document.querySelector('body').appendChild(image);
+        return image;
     }
 }
 
-setInterval(function () {
-    new Snack(randomBetween(5, 10))
-}, 5000)
+
+class Ryba {
 
 
-function randomBetween(min, max) {
-    return Math.floor(Math.random() * (max - min) + min)
+    constructor(element, imageSrc) {
+        this.ryba = document.getElementById(element);
+        this.food = [];
+        this.createFish(imageSrc)
+        this.foodThread();
+        this.eatThread();
+    }
+
+    createFish(imageSrc) {
+        let image = new Image();
+        image.src = imageSrc;
+        image.classList.add('ryba');
+        image.id = 'ryba';
+        document.querySelector('body').appendChild(image);
+    }
+
+    foodThread() {
+        this.createFood()
+        setTimeout(() => {
+            this.foodThread();
+        }, 5000);
+    }
+
+    createFood() {
+        this.food.push(new Snack(randomBetween(5, 10)));
+        console.log(this.food);
+    }
+
+    eatThread() {
+        const range = 100;
+        const ImageOffsetX = - 60;
+        const ImageOffsetY = - 50;
+        this.food.forEach(element => {
+            const ryba = document.getElementById('ryba');
+            let rybaLeft = parseInt(ryba.style.left)
+            let rybaTop = parseInt(ryba.style.top)
+
+            console.log([rybaLeft, element.offsetLeft]);
+            if (rybaLeft + ImageOffsetX + range > element.offsetLeft && rybaLeft + ImageOffsetX - range < element.offsetLeft) {
+                if (rybaTop + ImageOffsetY + range > element.offsetTop && rybaTop + ImageOffsetY - range < element.offsetTop) {
+                    console.log('eat');
+                    this.eat(element);
+                }
+            }
+
+        });
+        setTimeout(() => {
+            this.eatThread();
+        }, 500);
+    }
+
+    eat(element) {
+        element.remove();
+    }
+
+
 }
+
+
+
+
+
+
+new Ryba('ryba', 'ryba.png');
