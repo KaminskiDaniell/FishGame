4ead1f14918d832c1cb62734f62e82b5be0bdf10
EVBlue <daniel.kaminski123@gmail.com>
2022-09-06 12:17:46 +0100

    FIRST COMMIT

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let array = []
+
 document.addEventListener('mousemove', function (e) {
     let body = document.querySelector('body')
     let circle = document.getElementById('ryba')
@@ -14,6 +16,8 @@ document.addEventListener('mousemove', function (e) {
 
     circle.style.left = left - 60 + 'px';
     circle.style.top = top - 50 + 'px';
+
+    Ryba.checkForJedzenie(array);
 });
 
 class Ryba {
@@ -22,7 +26,24 @@ class Ryba {
     constructor(element, imageSrc) {
         this.ryba = document.getElementById(element)
         this.image = imageSrc
-        document.getElementById(element).outerHTML = `<img src="${imageSrc}" alt="ryba" class="ryba" id="ryba">`
+        let image = new Image()
+        image.src = imageSrc;
+        image.classList.add('ryba');
+        image.id = 'ryba'
+
+        document.querySelector('body').appendChild(image)
+        // document.getElementById(element).outerHTML = `<img src="${imageSrc}" alt="ryba" class="ryba" id="ryba">`
+    }
+
+    static checkForJedzenie(array) {
+        const ryba = document.getElementById('ryba')
+        console.log(ryba)
+        array.forEach(e => {
+            console.log([e.offsetX, ryba.offsetX])
+            if (e.offsetX - 20 < ryba.offsetX && e.offsetX + 20 > ryba.offsetX) {
+                console.log("jestes blisko zarcia")
+            }
+        })
     }
 }
 
@@ -32,32 +53,30 @@ new Ryba('ryba', 'ryba.png')
 
 
 class Snack {
-    snack
-
     constructor(size) {
-        this.size = size
-
         let x = randomBetween(1, 100)
         let y = randomBetween(1, 100)
-        console.log(`X: ${x}, Y: ${y}, SIZE: ${size}`)
-        this.spawnSnack(x, y)
+        // console.log(`X: ${x}, Y: ${y}, SIZE: ${size}`)
+        this.spawnSnack(x, y, size)
     }
 
-    spawnSnack(X, Y) {
+    spawnSnack(X, Y, size) {
         let image = new Image()
         image.src = 'https://cdn.shopify.com/s/files/1/2555/1168/files/information-worm_2048x.png?v=1632120158'
-        image.style.top = `${X}vh`
+        image.style.top = `${X}vh`;
         image.style.position = 'absolute';
-        image.style.left = `${Y}vw`
+        image.style.left = `${Y}vw`;
+        image.style.width = "auto";
+        image.style.height = `${size}vh`;
+        image.id = `${Date.now()}`;
         document.querySelector('body').appendChild(image)
-
+        array.push(image);
     }
 }
 
 setInterval(function () {
-    new Snack(randomBetween(1, 10))
+    new Snack(randomBetween(5, 10))
 }, 5000)
-new Snack(10);
 
 
 function randomBetween(min, max) {
