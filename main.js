d2b600c25d353a77ca04360c8d654b05f96a2bff
TheGoldyOne <kubazlotek@gmail.com>
2022-09-08 08:45:25 +0200

    a

--- main.js
+++ main.js
@@ -230,35 +230,44 @@ class Ryba {
 new Ryba('ryba', 'ryba.png');
 
 
-var objImage = null;
-
-function init() {
-    objImage = document.getElementById("ryba");
-    objImage.style.position = "relative";
-    objImage.style.left = "0px";
-    objImage.style.top = "0px";
-}
-
-document.addEventListener("keydown", function (event) {
-    getKey(event);
-});
-
-function getKey(e) {
-    var key_code = e.which || e.keyCode;
-    switch (key_code) {
-        case 65: // a
-            objImage.style.left = parseInt(objImage.style.left) - 5 + "px";
-            break;
-        case 87: // w
-            objImage.style.top = parseInt(objImage.style.top) - 5 + "px";
-            break;
-        case 68: // d
-            objImage.style.left = parseInt(objImage.style.left) + 5 + "px";
-            break;
-        case 83: // s
-            objImage.style.top = parseInt(objImage.style.top) + 5 + "px";
-            break;
+function keyboard_module(onUpdate){
+    var kb = {};
+    var unicode_mapping = {};
+    document.onkeydown = function(e){
+        var unicode=e.charCode? e.charCode : e.keyCode
+        var key = getKey(unicode);
+        kb[key] = true;
+        if(onUpdate){
+            onUpdate(kb);
+        }
     }
+
+    document.onkeyup = function(e){
+        var unicode=e.charCode? e.charCode : e.keyCode
+        var key = getKey(unicode);
+        delete kb[key];
+        if(onUpdate){
+            onUpdate(kb);
+        }
+    }
+
+    function getKey(unicode){
+        if(unicode_mapping[unicode]){
+            var key = unicode_mapping[unicode];
+        }else{
+            var key= unicode_mapping[unicode] = String.fromCharCode(unicode);
+        }
+        return key;
+    }
+    return kb;
 }
 
-window.onload = init;
+function testing(kb){
+    console.log('These are the down keys', kb);
+}
+
+
+var keyboard = keyboard_module(testing);
+
+
+if(keyboard['K']){/*do something special */}
