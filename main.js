72f79216f2c37262386d346b436322bbedd9b585
TheGoldyOne <kubazlotek@gmail.com>
2022-09-08 08:17:27 +0200

    hook update

--- main.js
+++ main.js
@@ -13,11 +13,27 @@ document.addEventListener('mousemove', function (e) {
 
     ryba.style.left = `${e.clientX + ImageOffsetX}px`;
     ryba.style.top = `${e.clientY + ImageOffsetY}px`;
+
+    // updateRotation(e.clientX, e.clientY);
+
 });
 
 function randomBetween(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
+
+// const getVectorAngle = ([x1, y1], [x2, y2]) =>  {
+//     const x = x2 - x1
+//     const y = y2 - y1
+//     return (((Math.acos(y / Math.sqrt(x * x + y * y)) * (Math.sign(x) || 1)) * 180 / Math.PI) + 360) % 360
+// }
+
+
+// const updateRotation = (clientx, clienty) => {
+//     const ryba = document.getElementById('ryba');
+//     const angle = getVectorAngle([ryba.offsetLeft, ryba.offsetTop], [clientx, clienty]);
+//     ryba.style.transform = `rotate(${angle - 270}deg)`;
+// }
 
 
 class Snack {
@@ -53,10 +69,51 @@ class Snack {
     }
 
     move() {
-        this.snack.style.top  = `${parseInt(this.snack.style.top) + 1}vh`;
+        this.snack.style.top = `${parseInt(this.snack.style.top) + 1}vh`;
         const side = randomBetween(-1, 2);
         this.snack.style.left = `${parseInt(this.snack.style.left) + side}vw`;
     }
+
+}
+
+class Hook {
+
+    constructor(size) {
+        let x = randomBetween(1, 100);
+        let y = randomBetween(1, 100);
+        this.hook = this.spawnHook(x, y, size);
+        this.moveThread();
+        return this.hook;
+    }
+
+    spawnHook(X, Y, size) {
+        let image = new Image();
+        image.src =
+            'hak.png';
+        X = 1; //TODO do wyjebanias
+        image.style.top = `${X}vh`;
+        image.style.position = 'absolute';
+        image.style.left = `${Y}vw`;
+        image.style.width = 'auto';
+        image.style.height = `${size}vh`;
+        image.style.transition = 'all 0.5s';
+        image.classList.add("hook");
+        image.id = `${Date.now()}`;
+        document.querySelector('body').appendChild(image);
+        return image;
+    }
+
+    moveThread() {
+        this.move()
+        setTimeout(() => {
+            this.moveThread();
+        }, 50);
+    }
+
+    move() {
+        this.hook.style.top = `${parseInt(this.hook.style.top) + 1}vh`;
+    }
+
 
 }
 
@@ -65,11 +122,16 @@ class Ryba {
 
 
     constructor(element, imageSrc) {
-        this.ryba = document.getElementById(element);
+        this.element = element
         this.food = [];
         this.createFish(imageSrc)
         this.foodThread();
         this.eatThread();
+        this.sizeThread(this.getFish());
+    }
+
+    getFish() {
+        return document.getElementById(this.element)
     }
 
     createFish(imageSrc) {
@@ -80,31 +142,46 @@ class Ryba {
         document.querySelector('body').appendChild(image);
     }
 
+
     foodThread() {
+        let random = randomBetween(1, 100);
+
+        if (random > 80) {
+            this.createDanger();
+        }
+
+
         this.createFood()
+
+
         setTimeout(() => {
             this.foodThread();
         }, 1000);
     }
 
+
+    createDanger() {
+        this.food.push(new Hook(randomBetween(5, 10)))
+    }
+
     createFood() {
         this.food.push(new Snack(randomBetween(5, 10)));
-        console.log(this.food);
     }
 
     eatThread() {
+
         const range = 100;
         const ImageOffsetX = - 60;
         const ImageOffsetY = - 50;
+
         this.food.forEach(element => {
             const ryba = document.getElementById('ryba');
-            let rybaLeft = parseInt(ryba.style.left)
-            let rybaTop = parseInt(ryba.style.top)
 
-            console.log([rybaLeft, element.offsetLeft]);
-            if (rybaLeft + ImageOffsetX + range > element.offsetLeft && rybaLeft + ImageOffsetX - range < element.offsetLeft) {
-                if (rybaTop + ImageOffsetY + range > element.offsetTop && rybaTop + ImageOffsetY - range < element.offsetTop) {
-                    console.log('eat');
+            const [rybaPosLeft, rybaPosTop] = [parseInt(ryba.style.left), parseInt(ryba.style.top)]
+
+            if (rybaPosLeft + ImageOffsetX + range > element.offsetLeft && rybaPosLeft + ImageOffsetX - range < element.offsetLeft) {
+                if (rybaPosTop + ImageOffsetY + range > element.offsetTop && rybaPosTop + ImageOffsetY - range < element.offsetTop) {
+                    // console.log('eat');
                     this.eat(element);
                 }
             }
@@ -116,12 +193,34 @@ class Ryba {
     }
 
     eat(element) {
+        if (element.classList.contains("hook")) {
+            this.setScore(0)
+        } else {
+            this.addScore(1);
+        }
         element.remove();
-        this.addScore(1);
+        
     }
 
     addScore(score) {
-        document.getElementById('score').innerHTML =  parseInt(document.getElementById('score').innerHTML) + score;
+        document.getElementById('score').innerHTML = parseInt(document.getElementById('score').innerHTML) + score;
+    }
+
+    setScore(score) {
+        document.getElementById('score').innerHTML = parseInt(score);
+    }
+
+    getScore() {
+        return parseInt(document.getElementById('score').innerHTML)
+    }
+
+    sizeThread() {
+
+        this.getFish().style.maxWidth = `${50 + this.getScore()}px`;
+
+        setTimeout(() => {
+            this.sizeThread();
+        }, 100);
     }
 
 
@@ -133,3 +232,5 @@ class Ryba {
 
 
 new Ryba('ryba', 'ryba.png');
+
+
